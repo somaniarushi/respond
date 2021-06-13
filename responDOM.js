8aa213f101d4c802c6193e6ee85e25348c02d25b
Arushi Somani <arushisomani@berkeley.edu>
2021-06-12 23:31:45 -0700

    add todos to the dom renderer

--- responDOM.js
+++ responDOM.js
@@ -1,3 +1,6 @@
+// TODO: Add properties passed in through elements to the dom element being generated.
+// TODO: Add listeners passed in through elements to the dom element being generated.
+// TODO: Implement textNode.
 const render = (element, parent) => {
     const {type, props} = element;
     const dom = document.createElement(type);
